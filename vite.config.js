f98db357c905a514b2422162951b113bbe4f9438
Zadiel Salas <sadielsalas35@gmail.com>
2025-02-04 18:21:51 -0400

    first 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: '/templete-v1/', // Reemplaza 'templete-v1' con el nombre de tu repositorio
+	build: {
+		outDir: 'build', // O usa 'dist' si prefieres
+	},
+});
