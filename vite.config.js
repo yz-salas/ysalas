8abad40dbbeb4e631a2f1862d8bee6d54eb39b61
Zadiel Salas <sadielsalas35@gmail.com>
2025-02-06 20:15:48 -0400

    restructuracion de la carpeta dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/', // Nombre de tu repositorio
+	base: '/ysalas/', // Nombre de tu repositorio
 	build: {
 		outDir: 'dist', // Asegurar que la carpeta de salida es 'dist'
 	},
