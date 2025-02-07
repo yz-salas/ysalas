e7f1004858ce25f6f99942b0b780967f0e8e72fb
Zadiel Salas <sadielsalas35@gmail.com>
2025-02-06 20:01:36 -0400

    Commit Funcional

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/ysalas/', // Nombre de tu repositorio
+	base: '/', // Nombre de tu repositorio
 	build: {
 		outDir: 'dist', // Asegurar que la carpeta de salida es 'dist'
 	},
