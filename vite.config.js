915d9142b8e2afb41d31063e8abe6f6f06315bc3
Zadiel Salas <sadielsalas35@gmail.com>
2025-02-07 22:31:28 -0400

    Desplegando en GitHub Pages desde master

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ysalas/', // Debe coincidir con el nombre del repositorio
+  base: '/ysalas/', // Nombre del repositorio
+  build: {
+    outDir: 'docs', // Guardar el build en la carpeta 'docs'
+  },
 });
